c2ef21f309d3a6aef0c4aff5d5c773dbc2d83548
Saikiran Maddela <saikiranmaddela@Saikirans-MBP.attlocal.net>
2022-01-02 22:32:31 -0600

    Training now

--- main.py
+++ main.py
@@ -34,8 +34,9 @@ from torchvision import datasets, transforms
 from PIL import Image
 from Generator import *
 from Discriminator import *
+from tqdm import tqdm
 
-def load_dataset(dataset = 'cifar10'):
+def load_dataset(dataset = 'cifar10', batch_size = 32):
     data_root = 'data'
     data_root = os.path.abspath(os.path.expanduser(data_root))
     root_dir = os.path.join(data_root, dataset)
@@ -52,37 +53,44 @@ def load_dataset(dataset = 'cifar10'):
     # Use Torchvision to load CIFAR10 Dataset
     train_dataset = datasets.CIFAR10(root=root_dir, train=True, download=True, transform=transform_train)
     test_dataset = datasets.CIFAR10(root=root_dir, train=False, download=True, transform=transform_test)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size)
 
-    return train_dataset, test_dataset
+    return train_loader, test_loader
 
 def train():
     train_data, test_dataset = load_dataset()
     loss_f = torch.nn.BCELoss()
     epochs = 10
-    labels = [0, 1]
+    G_loss = []
+    D_loss = []
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')  
 
     generator = Generator()
     discriminator = Discriminator()
     opt_generator = optim.Adam(generator.parameters(), lr = 1e-2)
     opt_discriminator = optim.Adam(discriminator.parameters(), lr = 1e-2)
     for epoch in range(epochs):
-        for im, labels in enumerate(train_data):
+        for im, labels in tqdm(train_data):
+            im = im.to(device)
+            labels = labels.to(device)
+
             ###################################
             ####### Train Discriminator #######
             ###################################
             # Train with all real images
             discriminator.zero_grad()
-            label = torch.full((im.size(0)), 1)
+            label = torch.full((im.size(0),), 1, dtype = torch.float, device = device)
             output = discriminator(im)
-            loss_d = loss_f(output, label)
+            loss_d = loss_f(output.view(-1), label)
             loss_d.backward()
 
             # Train with all fake images
-            input = torch.rand(100, 32, 32)
-            fake = generator(input)
+            inp = torch.rand(32, 100, 1, 1, device = device)
+            fake = generator(inp)
             label.fill_(0)
-            output = discriminator(input)
-            loss_fake = loss_f(output, label)
+            output = discriminator(fake.detach())
+            loss_fake = loss_f(output.view(-1), label)
             loss_fake.backward()
 
             # Accumulate Loss
@@ -95,7 +103,12 @@ def train():
             generator.zero_grad()
             label.fill_(1)
             output = discriminator(fake)
+            loss_g = loss_f(output.view(-1), label)
+            loss_g.backward()
+            opt_generator.step()
 
-train_data, test_data = load_dataset()
-            
+            G_loss.append(loss_g)
+            D_loss.append(loss_d)
+
+train()            
             
